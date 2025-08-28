347782603e6915878da407dd9d5e9ab5ecd97d41
Brandon Scott <contact@brandonscott.me>
2025-08-27 20:06:41 -0400

    feat: round timestamp to nearest 15 mins, bulk insert 500 insertion commands at a time

--- scripts/sync2.rb
+++ scripts/sync2.rb
@@ -7,11 +7,18 @@ require 'sqlite3'
 require 'date'
 require 'json'
 require 'rest-client'
+require 'rspotify'
 
 MAX_RETRIES = 25
 
+
+
 $db = SQLite3::Database.new('spotify-data.db')
 $db.results_as_hash = true
+$timestamp = DateTime.now.to_time
+# This can be extracted into a utility function like `round_time`, see https://stackoverflow.com/a/449293
+$timestamp = $timestamp.min - ($timestamp.min % 15)
+$timestamp.to_i
 
 def main
 
@@ -19,21 +26,59 @@ def main
   create_artist_snapshots_table
   seed_initial_artist_ids
 
-  artist_ids = read_artist_ids
-  # authenticate
+  authenticate
 
+  artist_ids = read_artist_ids
+
+  insertions = []
+
+  artist_ids.each_slice(50) do |artist_ids_chunk|
+    insertions.concat(find_and_generate_artist_snapshot_commands(artist_ids_chunk))
+    insertions.clear if flush_insertion_commands_if_needed(insertions)
+  end
+
+  flush_insertion_commands(insertions)
+  insertions.clear
+
+end
+
+def find_and_generate_artist_snapshot_commands(artist_ids_chunk)
+  artists = RSpotify::Artist.find(artist_ids_chunk).compact
+
+  artists.map { |artist| generate_insert_artist_snapshot_command(artist) }
+end
+
+def generate_insert_artist_snapshot_command(artist)
+  row = [artist.id, $timestamp, artist.popularity, artist.followers['total']]
+  ['INSERT OR IGNORE INTO artist_snapshots (id, timestamp, popularity, followers) VALUES (?, ?, ?, ?);', row]
+end
+
+def flush_insertion_commands_if_needed(insertions)
+  return false unless insertions.length == 500
+
+  flush_insertion_commands(insertions)
+
+  true
+end
+
+def flush_insertion_commands(insertions)
+  return if insertions.empty?
+
+  $db.transaction
+  insertions.each do |insertion|
+    $db.execute(*insertion)
+  end
+  $db.commit
 end
 
 def read_artist_ids
   $db.results_as_hash = false
   total = $db.execute('SELECT COUNT(id) FROM artist_ids;')[0][0]
   $db.results_as_hash = true
-  puts "artist_ids total #{total}"
   chunk_size = (total / 24).floor
-  puts "chunk_size #{chunk_size}"
+  puts "artist_ids total #{total} chunk_size #{chunk_size}"
   limit = chunk_size
   offset = chunk_size * current_hour
-  puts "limit: #{limit} offset: #{offset} current_hour: #{current_hour}"
   artist_id_rows = $db.execute('SELECT id FROM artist_ids LIMIT ? OFFSET ?;', [limit, offset])
   artist_id_rows.map { |row| row['id'] }
 end
