f4751a358c107bd33e5adc6307fc1fb7c0bf2724
Brandon Scott <contact@brandonscott.me>
2025-08-27 21:02:51 -0400

    fix: link to answer that was used for rounding time

--- scripts/date_utils.rb
+++ scripts/date_utils.rb
@@ -3,7 +3,7 @@
 require 'date'
 
 # Rounds time down to the nearest minute interval
-# @see https://stackoverflow.com/a/449293
+# @see https://stackoverflow.com/a/449322
 def round_time(time, minutes = 15)
   seconds = minutes * 60
   Time.at((time.to_f / seconds).floor * seconds).utc
