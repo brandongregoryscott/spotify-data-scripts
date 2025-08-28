eda06c3c02180a3676eb8d2a2b4376d32e8e920f
Brandon Scott <contact@brandonscott.me>
2025-08-28 18:50:54 -0400

    refactor: use module instance variable instead of global variable

--- scripts/sync.rb
+++ scripts/sync.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/GlobalVars
-
 require 'bundler/setup'
 require 'sqlite3'
 require 'date'
@@ -12,21 +10,21 @@ require_relative 'spotify_utils'
 require_relative 'db_utils'
 require_relative 'date_utils'
 
-$db = SQLite3::Database.new('spotify-data.db')
-$db.results_as_hash = true
-$timestamp = round_time(Time.now).to_i
+@db = SQLite3::Database.new('spotify-data.db')
+@db.results_as_hash = true
+@timestamp = round_time(Time.now).to_i
 
 def main
 
-  create_artist_ids_table($db)
-  create_artist_snapshots_table($db)
-  seed_initial_artist_ids($db)
+  create_artist_ids_table(@db)
+  create_artist_snapshots_table(@db)
+  seed_initial_artist_ids(@db)
 
   authenticate
 
   artist_ids = read_artist_ids
 
-  bulk_insert($db, artist_ids, method(:find_and_generate_artist_snapshot_commands))
+  bulk_insert(@db, artist_ids, method(:find_and_generate_artist_snapshot_commands))
 end
 
 def find_and_generate_artist_snapshot_commands(artist_ids_chunk)
@@ -36,22 +34,20 @@ def find_and_generate_artist_snapshot_commands(artist_ids_chunk)
 end
 
 def generate_insert_artist_snapshot_command(artist)
-  row = [artist.id, $timestamp, artist.popularity, artist.followers['total']]
+  row = [artist.id, @timestamp, artist.popularity, artist.followers['total']]
   ['INSERT OR IGNORE INTO artist_snapshots (id, timestamp, popularity, followers) VALUES (?, ?, ?, ?);', row]
 end
 
 def read_artist_ids
-  $db.results_as_hash = false
-  total = $db.get_first_value('SELECT COUNT(id) FROM artist_ids;')
-  $db.results_as_hash = true
+  @db.results_as_hash = false
+  total = @db.get_first_value('SELECT COUNT(id) FROM artist_ids;')
+  @db.results_as_hash = true
   chunk_size = (total / 24).floor
   puts "artist_ids total #{total} chunk_size #{chunk_size}"
   limit = chunk_size
   offset = chunk_size * current_hour_index
-  artist_id_rows = $db.execute('SELECT id FROM artist_ids LIMIT ? OFFSET ?;', [limit, offset])
+  artist_id_rows = @db.execute('SELECT id FROM artist_ids LIMIT ? OFFSET ?;', [limit, offset])
   artist_id_rows.map { |row| row['id'] }
 end
 
 main
-
-# rubocop:enable Style/GlobalVars
