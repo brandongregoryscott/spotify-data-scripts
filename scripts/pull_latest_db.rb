9aa0ecb636c65041fc4c403f43ab3a98ea240880
Brandon Scott <contact@brandonscott.me>
2025-08-28 20:13:38 -0400

    feat: simple script for pulling latest spotify data db by timestamped name

---
+++ scripts/pull_latest_db.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'aws-sdk-s3'
+
+S3_ENDPOINT = ENV['AWS_S3_ENDPOINT']
+ACCESS_KEY_ID = ENV['AWS_ACCESS_KEY_ID']
+SECRET_ACCESS_KEY = ENV['AWS_SECRET_ACCESS_KEY']
+
+@r2 = Aws::S3::Client.new(
+  access_key_id: ACCESS_KEY_ID,
+  secret_access_key: SECRET_ACCESS_KEY,
+  endpoint: S3_ENDPOINT,
+  region: 'auto'
+)
+
+def main
+  database_objects = @r2.list_objects(bucket: 'spotify-data', max_keys: 20).contents
+
+  databases_sorted_by_date = database_objects.sort_by { |database_object| parse_timestamp(database_object.key) }.reverse
+
+  last_uploaded_db = databases_sorted_by_date.first
+
+  @r2.get_object({ bucket: 'spotify-data', key: last_uploaded_db.key }, target: 'spotify-data.db')
+end
+
+# Parses the unix timestamp from the `spotify-data_123456789>.db` filename/key in S3
+def parse_timestamp(object_key)
+  timestamp_with_extension = object_key.split('_').last
+  timestamp = timestamp_with_extension.split('.db').first
+  Integer(timestamp)
+end
+
+main
