8fa76eeac5c3538b1db9ef310f20753004df9638
Brandon Scott <contact@brandonscott.me>
2025-08-30 18:43:13 -0400

    refactor: use uploaded db name instead of dropping timestamp

--- scripts/pull_latest_db.rb
+++ scripts/pull_latest_db.rb
@@ -21,7 +21,7 @@ def main
 
   last_uploaded_db = databases_sorted_by_date.first
 
-  @r2.get_object({ bucket: 'spotify-data', key: last_uploaded_db.key }, target: 'spotify-data.db')
+  @r2.get_object({ bucket: 'spotify-data', key: last_uploaded_db.key }, target: last_uploaded_db.key)
 end
 
 # Parses the unix timestamp from the `spotify-data_123456789>.db` filename/key in S3
