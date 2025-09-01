47843c62510208711cdf431ccbc9c2c989970597
Brandon Scott <contact@brandonscott.me>
2025-09-01 15:13:05 -0400

    refactor: extract function for retrieving databases descending by timestamp

--- scripts/pull_latest_db.rb
+++ scripts/pull_latest_db.rb
@@ -5,24 +5,13 @@ require 'aws-sdk-s3'
 require_relative 'storage_utils'
 
 def main
-  database_objects = @r2.list_objects(bucket: 'spotify-data', max_keys: 20).contents
-
-  databases_sorted_by_date = database_objects.sort_by { |database_object| parse_timestamp(database_object.key) }.reverse
-
-  last_uploaded_db = databases_sorted_by_date.first
+  last_uploaded_db = list_databases_desc.first
 
   puts("Downloading #{last_uploaded_db.key}...")
 
   @r2.get_object({ bucket: 'spotify-data', key: last_uploaded_db.key }, target: last_uploaded_db.key)
 
   puts("Downloaded #{last_uploaded_db.key}!")
-end
-
-# Parses the unix timestamp from the `spotify-data_123456789>.db` filename/key in S3
-def parse_timestamp(object_key)
-  timestamp_with_extension = object_key.split('_').last
-  timestamp = timestamp_with_extension.split('.db').first
-  Integer(timestamp)
 end
 
 main
