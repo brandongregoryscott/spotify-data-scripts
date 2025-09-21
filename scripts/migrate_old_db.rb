601570b54b6e1139d5a04f4fa13d42e2b3e1c322
Brandon Scott <contact@brandonscott.me>
2025-09-21 19:46:02 -0400

    fix: update migrate_old_db script to drop artists table + vacuum

--- scripts/migrate_old_db.rb
+++ scripts/migrate_old_db.rb
@@ -5,20 +5,20 @@ require 'sqlite3'
 require_relative 'db_utils'
 
 @db_name = db_name
-@db = SQLite3::Database.new(@db_name)
+@db = SQLite3::Database.new('_spotify-data_1751481126928.db')
 @db.results_as_hash = true
 
 def main
   @db.execute <<-SQL
-    ALTER TABLE artists RENAME TO artist_ids;
-  SQL
-
-  @db.execute <<-SQL
-    ALTER TABLE artist_ids DROP COLUMN name;
+    DROP TABLE artists;
   SQL
 
   @db.execute <<-SQL
     DROP TABLE git_history;
+  SQL
+
+  @db.execute <<-SQL
+    VACUUM;
   SQL
 end
 
